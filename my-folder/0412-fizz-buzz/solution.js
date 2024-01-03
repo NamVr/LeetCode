08aa0afd23c31baf1d1a82bb8e31ebd41ab0f6e7
Naman Vrati <info@namanvrati.me>
2024-01-03 20:52:44 +0000

    [LeetCode Sync] Runtime - 64 ms (31.75%), Memory - 44.4 MB (100.00%)

--- my-folder/0412-fizz-buzz/solution.js
+++ my-folder/0412-fizz-buzz/solution.js
@@ -6,11 +6,18 @@ var fizzBuzz = function(n) {
     const arr = [];
 
     for (let i = 1; i <= n; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {arr.push("FizzBuzz");}
-        else if (i % 3 == 0) {arr.push("Fizz");}
-        else if (i % 5 == 0) {arr.push("Buzz");}
-        else {arr.push(i.toString());}
+        let result = "";
+
+        if (i % 3 === 0) {
+            result += "Fizz";
+        }
+        if (i % 5 === 0) {
+            result += "Buzz";
+        }
+
+        arr.push(result || i.toString());
     }
 
     return arr;
 };
+
