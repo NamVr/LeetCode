369cae7f759a482dd32daaae8f5e7cca9b1a079a
Naman Vrati <info@namanvrati.me>
2024-10-10 08:58:38 +0000

    [LeetCode Sync] Runtime - 54 ms (44.04%), Memory - 46.9 MB (39.36%)

---
+++ 1002-maximum-width-ramp/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> s;
+
+        for (int i = 0; i < n; i++) {
+            // first pass
+            if (s.empty() || nums[s.top()] > nums[i]) s.push(i);
+        }
+        
+        int maxWidth = 0;
+
+        for (int j = n-1; j >= 0; j--) {
+            // second pass reverse
+
+            while (!s.empty() && nums[s.top()] <= nums[j]) {
+                maxWidth = max(maxWidth, j-s.top());
+                s.pop();
+            }
+        }
+
+        return maxWidth;
+    }
+};
