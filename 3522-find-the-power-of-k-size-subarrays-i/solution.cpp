09c672b0e39d3fefd24eeff046ccbd060b68a246
Naman Vrati <info@namanvrati.me>
2024-11-16 11:31:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 32.9 MB (77.47%)

---
+++ 3522-find-the-power-of-k-size-subarrays-i/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> ans(n-k+1, -1);
+
+        for (int i = 0; i < n-k+1; i++) {
+            int j = i;
+            for (; j < i+k-1; j++) {
+                if (nums[j+1] != nums[j] + 1) break;
+            }
+            if (j == i+k-1) ans[i] = nums[i+k-1];
+        }
+
+        return ans;
+    }
+};
