13cfd18fb1c7f010e9798a9a6bd511941ca9a2a8
Naman Vrati <info@namanvrati.me>
2025-03-23 19:43:20 +0000

    [LeetCode Sync] Runtime - 397 ms (90.04%), Memory - 42.9 MB (46.57%)

--- 1250-longest-common-subsequence/solution.py
+++ 1250-longest-common-subsequence/solution.py
@@ -1,14 +1,16 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        m = len(text1)
-        n = len(text2)
-        dp = [[0] * (n+1) for _ in range(m+1)] # dp table of m+1 x n+1
+        m,n = len(text1), len(text2)
+        # Create DP table for n+1 X m+1 grid
+        # fill all values with zero initially
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
 
+        # bottom up DP approach, start with the last grid.
         for i in reversed(range(m)):
             for j in reversed(range(n)):
                 if text1[i] == text2[j]:
                     dp[i][j] = 1 + dp[i+1][j+1]
                 else:
                     dp[i][j] = max(dp[i+1][j], dp[i][j+1])
-        
+
         return dp[0][0]
