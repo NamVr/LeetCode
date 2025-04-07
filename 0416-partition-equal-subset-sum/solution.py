63df161df093cdbbdd3786e3152d0afa094d0998
Naman Vrati <info@namanvrati.me>
2025-04-07 10:23:47 +0000

    [LeetCode Sync] Runtime - 778 ms (36.50%), Memory - 17.9 MB (88.29%)

---
+++ 0416-partition-equal-subset-sum/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canPartition(self, nums):
+        total_sum = sum(nums)
+        if total_sum & 1: return False
+        half_sum = total_sum // 2
+        dp = [True] + [False]*half_sum
+        for num in nums:
+            for j in range(half_sum, num-1, -1):
+                dp[j] |= dp[j-num]
+        return dp[half_sum]
