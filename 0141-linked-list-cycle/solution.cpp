0544e937373d158209a2dc59c1b608bb304ea10c
Naman Vrati <info@namanvrati.me>
2025-08-05 04:43:04 +0000

    [LeetCode Sync] Runtime - 7 ms (82.60%), Memory - 11.7 MB (80.06%)

--- 0141-linked-list-cycle/solution.cpp
+++ 0141-linked-list-cycle/solution.cpp
@@ -1,18 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if (!head || !head->next) return false;
+        ListNode* slow = new ListNode();
+        ListNode* fast = new ListNode();
 
-        ListNode* slow = head;
-        ListNode* fast = head;
+        slow = head;
+        fast = head;
 
-        while (fast != nullptr && fast->next != nullptr) {
-            fast = fast->next->next;
+        while (fast != nullptr and fast->next != nullptr) {
             slow = slow->next;
-            if (slow == fast) return true;
+            fast = fast->next->next;
+
+            if (slow == fast) {
+                return true;
+            }
         }
 
-        return false; 
+        return false;
     }
 };
-
