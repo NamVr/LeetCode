85546e333a4916c5e791d22c23a451dd7565327d
Naman Vrati <info@namanvrati.me>
2025-08-13 17:39:56 +0000

    [LeetCode Sync] Runtime - 3 ms (11.35%), Memory - 17.8 MB (37.58%)

---
+++ 0342-power-of-four/solution.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and n & (n-1) == 0 and (n-1) % 3 == 0
+        # must be positive,
+        # must be a power of 2
+        # must be divisible by 3 with previous number
