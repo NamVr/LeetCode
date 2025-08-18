527adac052280b6b3e47dd1e0c0831cb0f897522
Naman Vrati <info@namanvrati.me>
2025-08-18 09:35:44 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 17.9 MB (65.53%)

--- 0078-subsets/solution.py
+++ 0078-subsets/solution.py
@@ -1,25 +1,27 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        # return all posible subsets from a given array.
+        # either include the element or exclude it.
+        # when index goes out of bounds, return the subset copy.
+
         res = []
 
-        # represents the current subset.
         subset = []
-
-        # this function will take decision for the given INDEX passed.
+        
         def dfs(i):
-            if i >= len(nums): # out of bounds, return base case.
+            # base condition: if the index goes out of bounds.
+            if i >= len(nums):
                 res.append(subset.copy())
                 return
             
-            # decision to include nums[i]
+            # make a choice. add this element?
             subset.append(nums[i])
             dfs(i+1)
 
-            # decision to not include nums[i]
-            subset.pop() # remove the added append from previous recursive call.
+            # cleanup choice: remove this element.
+            subset.pop()
             dfs(i+1)
 
-        # call the function from base index 0
-        dfs(0)
+        dfs(0) # start the backtracking at first index.
 
         return res
