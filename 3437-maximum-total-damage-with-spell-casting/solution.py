9a64214aa8aaed693f8fb032311b0fde9ab598b2
Naman Vrati <info@namanvrati.me>
2025-10-11 03:21:15 +0000

    [LeetCode Sync] Runtime - 481 ms (67.06%), Memory - 39.8 MB (89.10%)

---
+++ 3437-maximum-total-damage-with-spell-casting/solution.py
@@ -0,0 +1,34 @@
+class Solution:
+    def maximumTotalDamage(self, power):
+        damageFrequency = defaultdict(int)
+        for damage in power:
+            damageFrequency[damage] += 1
+
+        uniqueDamages = sorted(damageFrequency.keys())
+        totalUniqueDamages = len(uniqueDamages)
+        maxDamageDP = [0] * totalUniqueDamages
+
+        maxDamageDP[0] = uniqueDamages[0] * damageFrequency[uniqueDamages[0]]
+
+        for i in range(1, totalUniqueDamages):
+            currentDamageValue = uniqueDamages[i]
+            currentDamageTotal = currentDamageValue * damageFrequency[currentDamageValue]
+
+            maxDamageDP[i] = maxDamageDP[i - 1]
+
+            previousIndex = i - 1
+            while (previousIndex >= 0 and 
+                   (uniqueDamages[previousIndex] == currentDamageValue - 1 or 
+                    uniqueDamages[previousIndex] == currentDamageValue - 2 or 
+                    uniqueDamages[previousIndex] == currentDamageValue + 1 or 
+                    uniqueDamages[previousIndex] == currentDamageValue + 2)):
+                previousIndex -= 1
+
+            if previousIndex >= 0:
+                maxDamageDP[i] = max(maxDamageDP[i], maxDamageDP[previousIndex] + currentDamageTotal)
+            else:
+                maxDamageDP[i] = max(maxDamageDP[i], currentDamageTotal)
+
+        return maxDamageDP[totalUniqueDamages - 1]
+
+        
