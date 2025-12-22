5962dcfe560d441e659466f81644f05576866a37
Naman Vrati <info@namanvrati.me>
2025-12-22 10:22:47 +0000

    [LeetCode Sync] Runtime - 710 ms (44.64%), Memory - 0.0B (100.00%)

---
+++ 1462-list-the-products-ordered-in-a-period/solution.sql
@@ -0,0 +1,7 @@
+# 23BCT10010 WWC _ G1
+
+SELECT p.product_name AS product_name, sum(o.unit) AS unit FROM Products p
+JOIN Orders o USING (product_id)
+WHERE YEAR(o.order_date)='2020' AND MONTH(o.order_date)='02'
+GROUP BY p.product_id
+HAVING SUM(o.unit)>=100
