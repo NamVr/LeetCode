243a176900990e0772380cfd6d20316f1f44db3b
Naman Vrati <info@namanvrati.me>
2025-12-28 11:59:02 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.3 MB (51.20%)

---
+++ 1476-count-negative-numbers-in-a-sorted-matrix/solution.py
@@ -0,0 +1,15 @@
+class Solution:
+    def countNegatives(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        res = 0
+
+        i,j = m-1, 0
+
+        while i>=0 and j<n:
+            if grid[i][j] < 0: #negative
+                res += n-j
+                i -= 1
+            else:
+                j += 1
+        
+        return res
