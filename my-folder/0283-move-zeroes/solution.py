46d596d2ee4603f877134374f04952338730d5e2
Naman Vrati <info@namanvrati.me>
2026-03-28 18:46:05 +0000

    [LeetCode Sync] Runtime - 3 ms (82.53%), Memory - 20.5 MB (23.43%)

---
+++ my-folder/0283-move-zeroes/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        l = 0
+
+        # r tracks 0 index
+        # l tracks current search space
+
+        for r in range(len(nums)):
+            if nums[r] != 0:
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1 # one zero is replaced
