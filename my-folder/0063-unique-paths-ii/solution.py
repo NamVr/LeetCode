f26a0882f1e66283856b4115537291358064297c
Naman Vrati <info@namanvrati.me>
2026-04-17 21:55:20 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 19.3 MB (63.71%)

--- my-folder/0063-unique-paths-ii/solution.py
+++ my-folder/0063-unique-paths-ii/solution.py
@@ -2,20 +2,16 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid):
         m, n = len(obstacleGrid), len(obstacleGrid[0])
         
-        if obstacleGrid[0][0] == 1 or obstacleGrid[-1][-1] == 1:
-            return 0
-        
-        grid = [[0] * n for _ in range(m)]
-        grid[0][0] = 1 # start from 1
+        dp = [0] * n
+        if obstacleGrid[0][0] == 0: # space => possible to continue
+            dp[0] = 1
         
         for i in range(m):
             for j in range(n):
-                if obstacleGrid[i][j] == 1: # if obstacle
-                    grid[i][j] = 0 # make it unreachable
-                else:
-                    if i > 0: # only if left column exists
-                        grid[i][j] += grid[i-1][j]
-                    if j > 0: # only if upper row exists
-                        grid[i][j] += grid[i][j-1]
+                # obstacle or not.
+                if obstacleGrid[i][j] == 1:
+                    dp[j] = 0
+                elif j > 0:
+                    dp[j] += dp[j-1]
         
-        return grid[-1][-1]
+        return dp[-1]
