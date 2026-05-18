54857a431f13fd9575faedc6cbf9965c1fdac0ef
Naman Vrati <info@namanvrati.me>
2026-05-18 00:42:26 +0000

    [LeetCode Sync] Runtime - 249 ms (11.05%), Memory - 33.6 MB (59.61%)

---
+++ my-folder/1447-jump-game-iv/solution.py
@@ -0,0 +1,36 @@
+class Solution:
+  def minJumps(self, arr: List[int]) -> int:
+    n = len(arr)
+    # {num: indices}
+    graph = collections.defaultdict(list)
+    steps = 0
+    q = collections.deque([0])
+    seen = {0}
+
+    # Create graph where keys are elements and values are their indices in arr
+    for i, a in enumerate(arr):
+      graph[a].append(i)
+
+    # BFS
+    while q:
+      # Process all nodes at current level
+      for _ in range(len(q)):
+        i = q.popleft()
+        # If last index is reached, return number of steps
+        if i == n - 1:
+          return steps
+        seen.add(i)
+        u = arr[i]
+        # Add adjacent indices to graph
+        if i + 1 < n:
+          graph[u].append(i + 1)
+        if i - 1 >= 0:
+          graph[u].append(i - 1)
+        # Process all adjacent nodes
+        for v in graph[u]:
+          if v in seen:
+            continue
+          q.append(v)
+        # Clear indices in graph to avoid revisiting
+        graph[u].clear()
+      steps += 1
